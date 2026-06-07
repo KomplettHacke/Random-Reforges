6ca043659bab9ffa4e71f1fd8daab5afe5daf68f
Pille <108745644+KomplettHacke@users.noreply.github.com>
2026-06-07 22:52:11 +0200

    Enhanced CuriosLoader with comments
    Created a separate file for CuriosAPI loading to mitigate crashes when CuriosAPI is not installed.

--- src/main/java/de/randomreforges/curios/CuriosLoader.java
+++ src/main/java/de/randomreforges/curios/CuriosLoader.java
@@ -3,7 +3,9 @@ package de.randomreforges.curios;
 import net.minecraftforge.common.MinecraftForge;
 
 
-//just a fallback to make sure that curio doesnt lead to crashes if its not loaded
+/*****************************************************************************************************************************************************************
+Not sure what exactly changed, but creating an own file just for CuriosAPI loading helped with some crashes if CuriosAPI was not installed
+*****************************************************************************************************************************************************************/
 public class CuriosLoader {
     public static void register() {
         MinecraftForge.EVENT_BUS.register(CurioAttributeEvents.class);

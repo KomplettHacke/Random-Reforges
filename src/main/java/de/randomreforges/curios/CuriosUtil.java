1b13e7d263f2717b2ba9db074b0d9f3ec905a6ad
Pille <108745644+KomplettHacke@users.noreply.github.com>
2026-06-07 22:54:20 +0200

    Enhanced CuriosUtil with comments
    Added a detailed comment explaining the purpose of the isCurioItem method.

--- src/main/java/de/randomreforges/curios/CuriosUtil.java
+++ src/main/java/de/randomreforges/curios/CuriosUtil.java
@@ -3,6 +3,11 @@ package de.randomreforges.curios;
 import net.minecraft.world.item.ItemStack;
 
 public class CuriosUtil {
+    /*****************************************************************************************************************************************************************
+    Helper function to check if an item is a CuriosAPI item
+
+    Needed to apply reforge attributes manually only to non-curio items and let CuriosAPI apply the reforges
+    *****************************************************************************************************************************************************************/
     public static boolean isCurioItem(ItemStack stack) {
         return stack.getTags().anyMatch(tag ->
             tag.location().getNamespace().equals("curios"));
